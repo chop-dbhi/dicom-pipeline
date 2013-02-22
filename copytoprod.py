8f7f32c6804869880998ef4ae0658147858132f0
Jeffrey Miller <jeffmax@gmail.com>
2013-02-22 10:54:04 -0500

    Minor fix.

--- copytoprod.py
+++ copytoprod.py
@@ -15,6 +15,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from dicom_models.staging.models import RadiologyStudy as RadStudy
 from dicom_models.production.models import RadiologyStudy, DataAvailability
 
+
 s_studies = RadStudy.objects.using('staging').filter(image_published=True)
 count = 0
 for study in s_studies:
@@ -22,7 +23,7 @@ for study in s_studies:
     study_in_prod = RadiologyStudy.objects.using('production').filter(study_uid=study.study_uid)
     if len(study_in_prod) == 0:
         new_prod_study = RadiologyStudy(study_uid = study.study_uid, modality=study.modality, sop_class=study.sop_class, number_of_series=study.number_of_series, total_images=study.total_images)
-    else: 
+    else:
         new_prod_study = study_in_prod[0]
         new_prod_study.modality = study.modality
         new_prod_study.sop_class = study.sop_class
@@ -31,15 +32,14 @@ for study in s_studies:
 
     new_prod_study.encounter_id = study.encounter_id
     new_prod_study.patient_id = study.encounter.patient_id
-    new_prod_study.save()	
+    new_prod_study.save()
 
     print "New study %s" % study.study_uid
     count+= 1
 
 print "%d new studies." % count
 
-data = DataAvailability.objects.all()
+data = DataAvailability.objects.using('production').all()
 for d in data:
     d.reset_calculated_fields(save=True)
-
 
